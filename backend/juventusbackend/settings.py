a04ac1fb3f8ce81f976b46022541a0bedffd2505
NicoleMariah186 <nicolemariah256@gmail.com>
2025-11-08 16:17:56 +0300

    redeploying on render

--- backend/juventusbackend/settings.py
+++ backend/juventusbackend/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-local-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'juventus-pieces.onrender.com']
+ALLOWED_HOSTS = [ 'https://juventus-pieces.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
